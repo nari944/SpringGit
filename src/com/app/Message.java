9cf956a3194fd39002fe8fd8d3d0e8d4d75fe754
narendar <narendar@LAPTOP-K191D7UH>
2021-01-29 11:48:37 +0530

    7th update

--- src/com/app/Message.java
+++ src/com/app/Message.java
@@ -3,8 +3,8 @@ package com.app;
 public class Message {
 
 	public static void main(String[] args) {
-     System.out.println("RRRRRRR");
-     System.out.println("EEEEE");
+		int a=10;
+     System.out.println("RRRRRRR" +a);
 	}
 
 }
